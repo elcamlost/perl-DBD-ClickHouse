931c8b42af445e076aa8f09e1824393b04fe51e2
Ilya Rassadin <elcamlost@gmail.com>
2016-07-26 16:03:03 +0300

    Change inner logic inside ClickHouse class

--- lib/ClickHouse.pm
+++ lib/ClickHouse.pm
@@ -4,79 +4,116 @@ use strict;
 use warnings FATAL => 'all';
 our $VERSION = '0.01';
 
-use ClickHouse::Default;
-
 use Net::HTTP;
 use URI;
 use URI::Escape;
 use URI::QueryParam;
-use Try::Tiny;
+use Carp;
+use Scalar::Util qw/looks_like_number/;
+
+our $AUTOLOAD;
 
 sub new {
     my ($class, %opts) = @_;
     my $self = bless {}, $class;
-
-    $opts{'host'}     ||= 'localhost';
-    $opts{'port'}     ||= ClickHouse::Default::PORT();
-    $opts{'database'} ||= ClickHouse::Default::DATABASE();
-    $opts{'user'}     ||= ClickHouse::Default::USER();
-
-    for my $option_name (qw/password user host port database/) {
-        if (defined $opts{$option_name}) {
-            $self->{"_$option_name"} = $opts{$option_name};
-        }
-    }
-    $self->{'_builder'} = \&_builder;
-
-    $self->_connect();
+    $self->_init(%opts);
     return $self;
 }
 
-sub _builder {
-    my ($self) = @_;
-    delete $self->{'_socket'};
-    delete $self->{'_uri'};
-
-    # create Net::HTTP object
-    my $socket = Net::HTTP->new(
-        'Host'        => $self->{'_host'},
-        'PeerPort'    => $self->{'_port'},
-        'HTTPVersion' => '1.1',
-
-    ) or die "Can't connect: $@";
-
-    # create URI object
-    my $uri = URI->new(sprintf ("http://%s:%d/?database=%s", $self->{'_host'}, $self->{'_port'}, $self->{'_database'}));
-    $uri->query_param('user' => $self->{'_user'});
-    $uri->query_param('password' => $self->{'_password'}) if $self->{'_password'};
-
-    $self->{'_socket'} = $socket;
-    $self->{'_uri'} = $uri;
-
-    return 1;
-
-}
-
-sub _connect {
-    my ($self) = @_;
-    $self->_builder($self);
-    return 1;
-}
-
-sub _query {
-    my ($self, $cb) = @_;
-    return &try (
-        $cb,
-        catch {
-            $self->_connect();
-            $cb->();
-        }
+{
+    my %_attrs = (
+        '_host'       => 'localhost',
+        '_port'       => 8123,
+        '_database'   => 'default',
+        '_user'       => ':',
+        '_password'   => 48,
+        '_keep_alive' => 1,
+        '_format'     => 'TabSeparated',
+        '_socket'     => undef,
+        '_uri'        => undef,
     );
+
+    #
+    # CLASS METHODS
+    #
+    # Returns a copy of the instance.
+    sub _clone {
+        my ($self)  = @_;
+        my ($clone) = {%$self};
+        bless( $clone, ref $self );
+        return ($clone);
+    }
+
+    # Verify that an attribute is valid (called by the AUTOLOAD sub)
+    sub _accessible {
+        my ( $self, $name ) = @_;
+        if ( exists $_attrs{$name} ) {
+
+            #$self->verbose("attribute $name is valid");
+            return 1;
+        }
+        else { return 0; }
+    }
+
+    # Initialize the object (only called by the constructor)
+    sub _init {
+        my ( $self, %args ) = @_;
+
+        foreach my $key ( keys %_attrs ) {
+            $key =~ s/^_+//;
+            if ( defined ($args{$key}) && $self->_accessible( "_$key" ) ) {
+                $self->{"_$key"} = $args{$key};
+            }
+            else {
+                $self->{"_$key"} = $_attrs{"_$key"};
+            }
+        }
+        # create Net::HTTP object
+        my $socket = Net::HTTP->new(
+            'Host'        => $self->{'_host'},
+            'PeerPort'    => $self->{'_port'},
+            'HTTPVersion' => '1.1',
+            'KeepAlive'   => $self->{'_keep_alive'},
+        ) or die "Can't connect: $@";
+
+        # create URI object
+        my $uri = URI->new(sprintf ("http://%s:%d/?database=%s", $self->{'_host'}, $self->{'_port'}, $self->{'_database'}));
+        $uri->query_param('user' => $self->{'_user'});
+        $uri->query_param('password' => $self->{'_password'}) if $self->{'_password'};
+
+        $self->{'_socket'} = $socket;
+        $self->{'_uri'} = $uri;
+
+        return 1;
+    }
 }
 
+sub ClickHouse::AUTOLOAD {
+    no strict 'refs';
+    my ( $self, $value ) = @_;
+    if ( ( $AUTOLOAD =~ /.*::_get(_\w+)/ ) && ( $self->_accessible($1) ) ) {
+        my $attr_name = $1;
+        *{$AUTOLOAD} = sub { return $_[0]->{$attr_name} };
+        return ( $self->{$attr_name} );
+    }
+    if ( $AUTOLOAD =~ /.*::_set(_\w+)/ && $self->_accessible($1) ) {
+        my $attr_name = $1;
+        *{$AUTOLOAD} = sub { $_[0]->{$attr_name} = $_[1]; return; };
+        $self->{$1} = $value;
+        return;
+    }
+    croak "No such method: $AUTOLOAD";
+}
+
+sub DESTROY {}
 
 sub disconnect {
-    # закрывает соединение
+    my ($self) = @_;
+    my $socket = $self->_get_socket();
+    $socket->keep_alive(0);
+    $self->ping();
+
+    return 1;
 }
 
 
@@ -84,12 +121,10 @@ sub disconnect {
 sub select {
     my ($self, $query) = @_;
 
-    return $self->_query(sub {
-        my $query_url = $self->_construct_query_uri($query);
+    my $query_url = $self->_construct_query_uri($query);
 
-        $self->{'_socket'}->write_request('GET' => $query_url);
-        return $self->_parse_response();
-    });
+    $self->_get_socket()->write_request('GET' => $query_url);
+    return $self->_parse_response();
 
 }
 
@@ -102,23 +137,22 @@ sub select_value {
 
 sub do {
     my ($self, $query, @values) = @_;
-    return $self->_query(sub {
-        my $query_url = $self->_construct_query_uri($query);
-        my $post_data = scalar @values ? join (",", map { "(" . join (",", @{ $_ }) . ")" } @values) : "\n" ;
+    my $query_url = $self->_construct_query_uri($query);
+    my $post_data = scalar @values ? join (",", map { "(" . join (",", @{ $_ }) . ")" } @values) : "\n" ;
 
-        $self->{'_socket'}->write_request('POST' => $query_url, $post_data);
-        return $self->_parse_response();
-    });
+    $self->_get_socket()->write_request('POST' => $query_url, $post_data);
+    return $self->_parse_response();
 
 }
 
 sub ping {
     my ($self) = @_;
-    eval {
-        $self->{_socket}->write_request('GET' => '/');
+
+    my ($code) = eval {
+        $self->_get_socket()->write_request('GET' => '/');
+        $self->_get_socket()->read_response_headers();
     };
 
-    my ($code) = eval { $self->{'_socket'}->read_response_headers() };
     if ($@) {
         return 0;
     }
@@ -134,7 +168,7 @@ sub ping {
 
 sub _parse_response {
     my ($self) = @_;
-    my ($code, $mess) = $self->{'_socket'}->read_response_headers();
+    my ($code, $mess) = $self->_get_socket()->read_response_headers();
     if ($code == 200 ) {
         return _formaty_query_result( $self->_read_body() );
     } else {
@@ -150,7 +184,7 @@ sub _read_body {
     my @response;
     while (1) {
         my $buf;
-        my $n = $self->{'_socket'}->read_entity_body($buf, 1024);
+        my $n = $self->_get_socket()->read_entity_body($buf, 1024);
         die "can't read response: $!" unless defined $n;
         last unless $n;
         push @response, split (/\n/, $buf);
@@ -167,7 +201,7 @@ sub _formaty_query_result {
 sub _construct_query_uri {
     my ($self, $query) = @_;
 
-    my $query_uri = $self->{'_uri'}->clone();
+    my $query_uri = $self->_get_uri()->clone();
     $query_uri->query_param('query' => $query);
 
     return $query_uri->as_string();
