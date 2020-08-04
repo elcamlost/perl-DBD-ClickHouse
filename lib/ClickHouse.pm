c266ab2595f7dc21867088a1301746fb572af43c
Ilya Rassadin <elcamlost@gmail.com>
2020-08-04 12:54:31 +0300

    check socket still exists while disconnecting

--- lib/ClickHouse.pm
+++ lib/ClickHouse.pm
@@ -4,7 +4,7 @@ use 5.010;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Net::HTTP;
 use URI;
@@ -89,8 +89,8 @@ sub new {
             'Host'        => $self->{'_host'},
             'PeerPort'    => $self->{'_port'},
             'HTTPVersion' => '1.1',
-            'KeepAlive'   =>  $self->{'_keep_alive'},
-            'Timeout'    =>  $self->{'_timeout'},
+            'KeepAlive'   => $self->{'_keep_alive'},
+            'Timeout'     => $self->{'_timeout'},
 
         ) or die "Can't connect: $@";
 
@@ -145,10 +145,10 @@ sub DESTROY {}
 
 sub disconnect {
     my ($self) = @_;
-    my $socket = $self->_get_socket();
-    $socket->keep_alive(0);
-    $self->ping();
-
+    if (my $socket = $self->_get_socket()) {
+        $socket->keep_alive(0);
+        $self->ping();
+    }
     return 1;
 }
 
@@ -319,7 +319,7 @@ ClickHouse - Database driver for Clickhouse OLAP Database
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 
 
